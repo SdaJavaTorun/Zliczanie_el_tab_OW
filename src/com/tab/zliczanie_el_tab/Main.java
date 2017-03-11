4e67464a3698b6800f8ae750ede950d227b31e1c
olwj <al.wajgert@gmail.com>
2017-03-11 10:04:17 +0100

    add files

---
+++ src/com/tab/zliczanie_el_tab/Main.java
@@ -0,0 +1,22 @@
+
+	package com.tab.zliczanie_el_tab;
+
+        public class Main {
+
+            private static int a[];
+
+            public static void main(String[] args) {
+
+                int[] tab = {4, 1, 6, 1, 3};
+                int wynik = 0;
+
+                for (int i=0; i<tab.length; i++) {
+
+                    wynik += tab[i];
+
+                }
+                System.out.println("Suma el. tab. to " +wynik);
+            }
+        }
+
+
